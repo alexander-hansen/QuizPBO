8254ad34d14c106033d8c376769c13c7be19ddff
Hansen <Hansen @Hansen>
2020-11-18 12:40:07 +0700

    create ViewLihatDataJurusan

---
+++ src/View/ViewLihatDataJurusan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author V for Vladimir
+ */
+public class ViewLihatDataJurusan {
+    JFrame frame = new JFrame("Lihat Data Semua Jurusan");
+    
+}
